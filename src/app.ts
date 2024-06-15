70c053ec1a223caf9174657b148056eaa86fb27d
jhon jurado <jhonedisonjurado59@gmail.com>
2024-06-15 17:19:57 -0500

    Creacion de Rutas de la Api

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("Hola mundo");
+import { Server } from "./presentation/server";
+import { envs } from "./config";
+import { AppRoutes } from "./presentation/routes";
+
+(() => {
+    main()
+})()
+
+async function main() {
+
+
+
+    new Server({
+        port: envs.PORT,
+        routes: AppRoutes.routes
+    }).start()
+}
