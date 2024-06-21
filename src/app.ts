2752bfe8f2c7ba3fc10bf73697307a74fa608bbc
jhonjurado <jhonedisonjurado57@gmail.com>
2024-06-21 16:33:16 -0500

    conectar base de datos mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Server } from "./presentation/server";
 import { envs } from "./config";
 import { AppRoutes } from "./presentation/routes";
+import { mongoDatabase } from "./data/mongodb";
 
 (() => {
     main()
@@ -8,7 +9,11 @@ import { AppRoutes } from "./presentation/routes";
 
 async function main() {
 
+await mongoDatabase.connect({
 
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl:envs.MONGO_URL,
+})
 
     new Server({
         port: envs.PORT,
