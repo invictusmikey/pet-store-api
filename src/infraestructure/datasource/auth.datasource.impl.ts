a93475ce0694572199abeeb192d40b3b40f40185
jhonjurado <jhonedisonjurado57@gmail.com>
2024-06-21 17:00:17 -0500

    creacion de usuario en base de datos mongoDB

--- src/infraestructure/datasource/auth.datasource.impl.ts
+++ src/infraestructure/datasource/auth.datasource.impl.ts
@@ -1,4 +1,11 @@
-import { AuthDataSource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
+;
+import { UserModel } from '../../data/models/users.model'
+import {
+    AuthDataSource,
+    CustomError,
+    RegisterUserDto,
+    UserEntity
+} from "../../domain"
 
 export class AuthDataSourceImpl implements AuthDataSource {
 
@@ -6,9 +13,19 @@ export class AuthDataSourceImpl implements AuthDataSource {
         const { name, email, password } = registerUserDto
 
         try {
-
+            const exists = await UserModel.findOne({ email });
+            if (exists) throw CustomError.badRequest("User already exists");
+      
+            // Crear el usuario
+            const user = await UserModel.create({
+              name: name,
+              email: email,
+              password: password,
+            });
+            // Guardamos el usuario en la base de datos
+            await user.save();
             return new UserEntity(
-                '1',
+                user.id,
                 name,
                 email,
                 password,
